4ff8e0ea11958af64b889a777e90968aac8b8a33
mwanginjamb <fnjambi@outlook.com>
2022-07-27 19:39:00 +0200

    Fix: remove apostrophies in sp path constructon

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -131,23 +131,21 @@ class SiteController extends Controller
                 $size = $bin ? getImageSizeFromString($bin) : null;
                 $ext = $size ? substr($size['mime'], 6) : 'png';
                 $img_file = Yii::$app->security->generateRandomString(5) . '.' . $ext;
-                file_put_contents($img_file, $bin);
-
-                //Upload to sharepoint
-
-                $LibraryParts = $this->getLibraryParts($model->polling_station);
-                \Yii::$app->sharepoint->sharepoint_attach($img_file, $LibraryParts);
-
-                // Create a record in Docs Table
-                $model->local_file_path = Url::home(true) . $img_file;
-                $pathParts = "//sites//DMS//" . env('SP_LIBRARY') . '//' . $LibraryParts . '//';
-                $pathParts = str_replace('.', "", $pathParts);
-                $path = $pathParts . $img_file;
-                $model->sharepoint_path = $path;
-                if (!$model->save()) {
-                    return [
-                        'errors' => $model->errors
-                    ];
+                if (file_put_contents($img_file, $bin)) {
+                    //Upload to sharepoint
+                    $LibraryParts = $this->getLibraryParts($model->polling_station);
+                    \Yii::$app->sharepoint->sharepoint_attach($img_file, $LibraryParts);
+                    // Create a record in Docs Table
+                    $model->local_file_path = Url::home(true) . $img_file;
+                    $pathParts = "//sites//DMS//" . env('SP_LIBRARY') . '//' . $LibraryParts . '//';
+                    $pathParts = str_replace('.', "", $pathParts);
+                    $path = $pathParts . $img_file;
+                    $model->sharepoint_path = $path;
+                    if (!$model->save()) {
+                        return [
+                            'errors' => $model->errors
+                        ];
+                    }
                 }
 
                 return [
@@ -169,7 +167,9 @@ class SiteController extends Controller
 
         $model = PollingCenter::findOne(['polling_station_code' => $pollingStationCode]);
         $path =  $model->county_name . "/" . $model->constituency_name . "/" . $model->caw_name . "/" . $model->polling_station_name . "/" . $model->polling_station_code;
-        $parts =  str_replace('.', "", $path);
+        $parts =  str_replace('.', "", $path); // remove full stops
+        $parts = str_replace('\'', '', $path);
+
         return $parts;
     }
 
